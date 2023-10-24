06e0e0147effe12ef953792eec6c7e3bbd43a23b
Philip Opiyo <opiyophilipo@gmail.com>
2023-10-24 05:24:32 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module Square """
+
+
+class Square:
+    """ Square class defined by (based on 0-square.py)
+
+        Attributes:(Private)
+            size (int): Size of square
+    """
+    def __init__(self, size):
+        """
+        Initialize method
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
