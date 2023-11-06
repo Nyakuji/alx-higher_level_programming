2d08697063aeb717d94dffff924702dc1a9b9cf2
Philip Opiyo <opiyophilipo@gmail.com>
2023-11-06 04:17:49 -0800

    task 2-6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    BaseGeometry module
+"""
+
+
+class BaseGeometry():
+    """
+        BaseGeometry Class (based on 5-base_geometry.py).
+    """
+    def area(self):
+        """
+            public instance
+            Raise:
+                Exception: Area not implemented
+
+        """
+        raise Exception("area() is not implemented")
