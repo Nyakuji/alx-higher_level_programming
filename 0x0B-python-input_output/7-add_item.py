3862a725090fe49688fe7fd2ced913d6c7495497
Philip Opiyo <opiyophilipo@gmail.com>
2023-11-07 05:02:33 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 open("add_item.json", "a")
 try:
-    a = load_from_json_file("add_item.json")
+    myList = load_from_json_file("add_item.json")
 except ValueError:
-    a = []
-save_to_json_file(l + sys.argv[1:], "add_item.json")
+    myList = []
+save_to_json_file(myList + sys.argv[1:], "add_item.json")
